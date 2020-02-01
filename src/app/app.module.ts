dc173f4a77158f9b61d3f2173dcefa519cc2ca75
wayne <e28s32is@gmail.com>
2020-02-02 00:06:10 +0800

    inital project (3mins)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
